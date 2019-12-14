6bfeea7c36dfd965910e34bdd70b74afec1c18cf
Samuel Gélineau <gelisam@simspace.com>
2019-12-14 18:49:53 -0500

    all together in a single file

--- src/FreePremonoidal.hs
+++ src/FreePremonoidal.hs
@@ -25,39 +25,158 @@ type FromList (bs :: [Type])
               (b :: Type)
   = ToList b bs
 
+data FromSet (as :: [Type])
+             (a :: Type)
+             where
+  FromSet :: GrabN xs '[] as
+          -> FromList xs a
+          -> FromSet as a
 
-data Action (k :: Type -> Type -> Type)
-            (as :: [Type])
-            (bs :: [Type])
+data FromSuperset (as :: [Type])
+                  (a :: Type)
+                  where
+  FromSuperset :: GrabN xs xs' as
+               -> FromList xs a
+               -> FromSuperset as a
+
+
+data Grab1 (x :: Type)
+           (as :: [Type])
+           (bs :: [Type])
+           where
+  Grab1 :: ( as ~ (left ++ right)
+           , bs ~ (left ++ '[x] ++ right)
+           )
+        => Proxy left -> Proxy x -> Proxy right
+        -> Grab1 x as bs
+
+data GrabN (xs :: [Type])
+           (as :: [Type])
+           (bs :: [Type])
             where
-  Action :: FromList as a
-         -> k a b
-         -> ToList b bs
-         -> Action k as bs
+  NilG  :: GrabN '[] '[] bs
+  ConsG :: Grab1 x as bs
+        -> GrabN xs bs cs
+        -> GrabN (x ': xs) as cs
+
+
+data Reuse1 (x :: Type)
+            (as :: [Type])
+            where
+  Reuse1 :: ( as ~ (left ++ '[x] ++ right)
+            )
+         => Proxy left -> Proxy x -> Proxy right
+         -> Reuse1 x as
+
+data ReuseN (xs :: [Type])
+            (as :: [Type])
+            where
+  NilR  :: ReuseN '[] as
+  ConsR :: Reuse1 x as
+        -> ReuseN xs as
+        -> ReuseN (x ': xs) as
+
+
+data ListAction (k :: Type -> Type -> Type)
+                (as :: [Type])
+                (bs :: [Type])
+                where
+  ListAction :: FromList as a
+             -> k a b
+             -> ToList b bs
+             -> ListAction k as bs
+
+data PortionAction (action :: [Type] -> [Type] -> Type)
+                   (as :: [Type])
+                   (bs :: [Type])
+                   where
+  PortionAction :: ( as ~ (left ++ as' ++ right)
+                   , bs ~ (left ++ bs' ++ right)
+                   )
+                => Proxy left
+                -> action as' bs'
+                -> Proxy right
+                -> PortionAction k as bs
+
+data GrabAction (action :: [Type] -> [Type] -> Type)
+                (as :: [Type])
+                (bs :: [Type])
+                where
+  GrabAction :: GrabN as' rest as
+             -> action as' bs'
+             -> GrabAction action as (bs' ++ rest)
+
+data ReuseAction (action :: [Type] -> [Type] -> Type)
+                 (as :: [Type])
+                 (bs :: [Type])
+                 where
+  ReuseAction :: ReuseN as' as
+              -> action as' bs'
+              -> ReuseAction action as (bs' ++ as)
+
+
+data FreeCategory (k :: Type -> Type -> Type)
+                  (a :: Type)
+                  (b :: Type)
+                  where
+  NilC  :: FreeCategory k a a
+  ConsC :: k a b
+        -> FreeCategory k b c
+        -> FreeCategory k a c
 
 data Actions (action :: [Type] -> [Type] -> Type)
              (as :: [Type])
              (bs :: [Type])
              where
-  Nil  :: Actions action as as
-  Cons :: ( as ~ (left ++ as' ++ right)
-          , bs ~ (left ++ bs' ++ right)
-          )
-       => Proxy left -> action as' bs' -> Proxy right
-       -> Actions action bs cs
-       -> Actions action as cs
+  NilA  :: Actions action as as
+  ConsA :: action as bs
+        -> Actions action bs cs
+        -> Actions action as cs
 
 -- Free premonoidal category
-data Free (k :: Type -> Type -> Type)
+data FreePremonoidal (k :: Type -> Type -> Type)
+                     (a :: Type)
+                     (b :: Type)
+                     where
+  FreePremonoidal
+    :: ToList a as
+    -> Actions (PortionAction (ListAction k)) as bs
+    -> FromList bs b
+    -> FreePremonoidal k a b
+
+
+-- Free symmetric premonoidal category
+data FreeSymmetric (k :: Type -> Type -> Type)
           (a :: Type)
           (b :: Type)
           where
-  Free
+  FreeSymmetric
     :: ToList a as
-    -> Actions (Action k) as bs
-    -> FromList bs b
-    -> Free k a b
+    -> Actions (GrabAction (ListAction k)) as bs
+    -> FromSet bs b
+    -> FreeSymmetric k a b
 
+-- Free semicartesian premonoidal category
+data FreeSemicartesian (k :: Type -> Type -> Type)
+                       (a :: Type)
+                       (b :: Type)
+                       where
+  FreeSemicartesian
+    :: ToList a as
+    -> Actions (GrabAction (ListAction k)) as bs
+    -> FromSuperset bs b
+    -> FreeSemicartesian k a b
+
+-- Free cartesian premonoidal category
+data FreeCartesian (k :: Type -> Type -> Type)
+                   (a :: Type)
+                   (b :: Type)
+                   where
+  FreeCartesian
+    :: ToList a as
+    -> Actions (ReuseAction (ListAction k)) as bs
+    -> FromSuperset bs b
+    -> FreeCartesian k a b
 
 
 main :: IO ()
