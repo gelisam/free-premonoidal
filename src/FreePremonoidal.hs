7ce65b7fe653d29cccd12d28062e255e5f50cc6e
Samuel Gélineau <gelisam@simspace.com>
2020-03-07 11:09:52 -0500

    instances

--- src/FreePremonoidal.hs
+++ src/FreePremonoidal.hs
@@ -3,6 +3,7 @@ module FreePremonoidal where
 
 import Prelude hiding (id, (.))
 
+import qualified Control.Arrow as K
 import Control.Category
 import Data.Kind (Type)
 import Data.Proxy
@@ -34,6 +35,46 @@ class Symmetric k => Semicartesian k where
 -- cartesian premonoidal category
 class Semicartesian k => Cartesian k where
   dup :: k a (a, a)
+
+
+instance Premonoidal (->) where
+  first  = K.first
+  second = K.second
+  introL a = ((), a)
+  introR a = (a, ())
+  elimL  ((), a) = a
+  elimR  (a, ()) = a
+  assocL (a, (b, c)) = ((a, b), c)
+  assocR ((a, b), c) = (a, (b, c))
+
+instance Symmetric (->) where
+  swap (a, b) = (b, a)
+
+instance Semicartesian (->) where
+  forget a = ()
+
+instance Cartesian (->) where
+  dup a = (a, a)
+
+
+instance Monad m => Premonoidal (K.Kleisli m) where
+  first  = K.first
+  second = K.second
+  introL = K.arr introL
+  introR = K.arr introR
+  elimL  = K.arr elimL
+  elimR  = K.arr elimR
+  assocL = K.arr assocL
+  assocR = K.arr assocR
+
+instance Monad m => Symmetric (K.Kleisli m) where
+  swap = K.arr swap
+
+instance Monad m => Semicartesian (K.Kleisli m) where
+  forget = K.arr forget
+
+instance Monad m => Cartesian (K.Kleisli m) where
+  dup = K.arr dup
 
 
 type family (++) (as :: [Type])
