9593b5d2e271036994127c614569b2b38abff7f3
Samuel Gélineau <gelisam@simspace.com>
2020-03-09 10:01:30 -0400

    split into smaller modules

---
+++ src/FreeCategory.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE GADTs, LambdaCase, PolyKinds, RankNTypes #-}
+module FreeCategory where
+
+import Prelude hiding (id, (.))
+
+import Control.Category
+import Data.Kind (Type)
+
+
+data FreeCategory (k :: i -> i -> Type)
+                  (a :: i)
+                  (b :: i)
+                  where
+  Id     :: FreeCategory k a a
+  (:>>>) :: k a b
+         -> FreeCategory k b c
+         -> FreeCategory k a c
+
+instance Category (FreeCategory k) where
+  id = Id
+  (.) = flip go where
+    go :: FreeCategory k a b
+       -> FreeCategory k b c
+       -> FreeCategory k a c
+    go Id           gs = gs
+    go (f :>>> fs) gs = f :>>> go fs gs
+
+runFreeCategory
+  :: Category r
+  => (forall x y. k x y -> r x y)
+  -> FreeCategory k a b -> r a b
+runFreeCategory runK = \case
+  Id        -> id
+  f :>>> fs -> runK f
+           >>> runFreeCategory runK fs
--- src/FreePremonoidal.hs
+++ src/FreePremonoidal.hs
@@ -1,27 +1,17 @@
-{-# LANGUAGE GADTs, LambdaCase, PolyKinds, RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE GADTs, KindSignatures, RankNTypes, ScopedTypeVariables #-}
 module FreePremonoidal where
-
-import Prelude hiding (id, (.))
 
 import Control.Category
 import Data.Kind (Type)
 
 import Consume
 import Divide
+import FreeCategory
 import List
 import Observe
 import Premonoidal
 import Tuple
 
-
-data FreeCategory (k :: i -> i -> Type)
-                  (a :: i)
-                  (b :: i)
-                  where
-  Id     :: FreeCategory k a a
-  (:>>>) :: k a b
-         -> FreeCategory k b c
-         -> FreeCategory k a c
 
 data FreePremonoidal (k :: Type -> Type -> Type)
                      (a :: Type)
@@ -63,25 +53,6 @@ data FreeCartesian (k :: Type -> Type -> Type)
     -> FromSuperset bs b
     -> FreeCartesian k a b
 
-
-instance Category (FreeCategory k) where
-  id = Id
-  (.) = flip go where
-    go :: FreeCategory k a b
-       -> FreeCategory k b c
-       -> FreeCategory k a c
-    go Id           gs = gs
-    go (f :>>> fs) gs = f :>>> go fs gs
-
-
-runFreeCategory
-  :: Category r
-  => (forall x y. k x y -> r x y)
-  -> FreeCategory k a b -> r a b
-runFreeCategory runK = \case
-  Id        -> id
-  f :>>> fs -> runK f
-           >>> runFreeCategory runK fs
 
 runFreePremonoidal
   :: forall r k a b. Premonoidal r
