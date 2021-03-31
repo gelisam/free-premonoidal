4f7e46355e1f63b2f5a53fdde287a5f12d2b42e1
Samuel Gélineau <gelisam+github@gmail.com>
2021-03-31 09:19:31 -0400

    improve docs, switch from first/second to widen

--- src/Control/Category/Premonoidal.hs
+++ src/Control/Category/Premonoidal.hs
@@ -10,37 +10,73 @@ import Data.Proxy
 import TypeLevel.Append
 
 
--- premonoidal category
+-- A Premonoidal category can be seen as a Category with more structure, or as
+-- Linear types with more restrictions.
 --
--- e.g. [a, b, cd]
---       | q1 :: q [a, b] ab
---      [ab, cd]
---           | q2 :: q cd [c, d]
---      [ab, c, d]
---       | q3 :: q ab []
---      [c, d]
---           | q4 :: q [] e
---      [c, d, e]
+-- In a Category, the output of each morphism is the input of the next
+-- morphism; that is, this input is completely consumed by the next morphism.
+-- With Premonoidal, a morphism is allowed to only consume a fraction of its
+-- input. The way this works is that there are two morphisms: the thin
+-- morphism, which only consumes a fraction of its input, and the wide
+-- morphism, which consumes the entire input, but does so in a way which leaves
+-- untouched the parts of the input which are not consumed by the thin
+-- morphism.
 --
--- true in a monoidal category but not in a premonoidal category:
---   (f *** id) >>> (id *** g) = f *** g = (id *** g) >>> (f *** id)
---   first f >>> second g  =  second g >>> first f
+-- In Premonoidal, the portion which is consumed must be a sub-list of
+-- consecutive elements of the input. Linear lifts that restrictions to allow
+-- any subset of the input. Premonoidal's restrictiveness is sometimes useful.
+-- For example, in https://github.com/gelisam/category-syntax#wire-diagrams,
+-- the values represent wires and we want to force the user to choose which
+-- wire passes over the other whenever the wires cross.
+--
+-- For example, the wire diagram
+--
+--     a  b     c  d
+--     |  |     |  |
+--      \ |     |  |
+--        \     |  |
+--        | \   |  |
+--       /   | /   |
+--      |    /     |
+--      |  / |     | _
+--      | |  |     /   \
+--      | |  |   / |    |
+--      | |  |  |  |    |
+--      | |  |  |  |    |
+--      b c  a  e  d    f
+--
+-- Can be represented as
+--
+-- >     -- [a, b, c, d]
+-- >     widen (Proxy @[])
+-- >           (over :: q [a, b] [b, a])
+-- >           (Proxy @[c, d])
+-- >     -- [b, a, c, d]
+-- > >>> widen (Proxy @[b])
+-- >           (under :: q [a, c] [c, a])
+-- >           (Proxy @[d])
+-- >     -- [b, c, a, d]
+-- > >>> widen (Proxy @[b, c, a, d])
+-- >           (cap :: q [] [e, f])
+-- >           (Proxy @[])
+-- >     -- [b, c, a, d, e, f]
+-- > >>> widen (Proxy @[b, c, a])
+-- >           (under :: q [d, e] [e, d])
+-- >           (Proxy @[f])
+-- >     -- [b, c, a, e, d, f]
+--
+-- Each morphism may perform arbitrary side-effects. This is different from a
+-- monoidal category, in which side-effects are forbidden because of the
+-- following law, which requires effects to commute:
+--
+-- >   (f *** id) >>> (id *** g) = f *** g = (id *** g) >>> (f *** id)
+-- >   first f >>> second g  =  second g >>> first f
+--
+-- The above law does _not_ need to hold for Premonoidal instances.
 class Category q
    => Premonoidal (q :: [k] -> [k] -> Type) where
-  first
-    :: q as bs
-    -> Proxy zs
-    -> q (as ++ zs) (bs ++ zs)
-  second
+  widen
     :: Proxy xs
     -> q as bs
-    -> q (xs ++ as) (xs ++ bs)
-
-
---instance Premonoidal (->) where
---  first  = K.first
---  second = K.second
---
---instance Monad m => Premonoidal (K.Kleisli m) where
---  first  = K.first
---  second = K.second
+    -> Proxy zs
+    -> q (xs ++ as ++ zs) (xs ++ bs ++ zs)
--- src/Control/Category/Premonoidal/Free.hs
+++ src/Control/Category/Premonoidal/Free.hs
@@ -26,63 +26,42 @@ data PremonoidalAtom
 type FreePremonoidal q = FreeCategory (PremonoidalAtom q)
 
 
-firstAtom
-  :: PremonoidalAtom q as bs
-  -> Proxy zs
-  -> PremonoidalAtom q (as ++ zs)
-                       (bs ++ zs)
-firstAtom (PremonoidalAtom xs q ys) zs
-  = go xs q ys zs
-  where
-    go :: forall q xs as bs ys zs
-        . Proxy xs
-       -> q as bs
-       -> Proxy ys
-       -> Proxy zs
-       -> PremonoidalAtom q ((xs ++ as ++ ys) ++ zs)
-                            ((xs ++ bs ++ ys) ++ zs)
-    go xs q ys zs
-     -- ((xs ++ _) ++ ys) ++ zs
-      = withAssoc @(xs ++ as) @ys @zs
-      $ withAssoc @(xs ++ bs) @ys @zs
-     -- (xs ++ _) ++ (ys ++ zs)
-      $ PremonoidalAtom xs q (appendP ys zs)
-
-secondAtom
-  :: Proxy xs
+widenAtom
+  :: Proxy ws
   -> PremonoidalAtom q as bs
-  -> PremonoidalAtom q (xs ++ as) (xs ++ bs)
-secondAtom xs (PremonoidalAtom ys q zs)
-  = go xs ys q zs
+  -> Proxy zs
+  -> PremonoidalAtom q (ws ++ as ++ zs)
+                       (ws ++ bs ++ zs)
+widenAtom ws (PremonoidalAtom xs q ys) zs
+  = go ws xs q ys zs
   where
-    go :: forall q xs ys as bs zs
-        . Proxy xs
-       -> Proxy ys
+    go :: forall q ws xs as bs ys zs
+        . Proxy ws
+       -> Proxy xs
        -> q as bs
+       -> Proxy ys
        -> Proxy zs
-       -> PremonoidalAtom q (xs ++ (ys ++ as ++ zs))
-                            (xs ++ (ys ++ bs ++ zs))
-    go xs ys q zs
-     -- xs ++ ((ys ++ as) ++ zs)
-      = withAssoc @xs @ys @as
-      $ withAssoc @xs @ys @bs
-     -- (xs ++ (ys ++ as)) ++ zs
-      $ withAssoc @xs @(ys ++ as) @zs
-      $ withAssoc @xs @(ys ++ bs) @zs
-     -- ((xs ++ ys) ++ as) ++ zs
-      $ PremonoidalAtom (appendP xs ys) q zs
+       -> PremonoidalAtom q (ws ++ (xs ++ as ++ ys) ++ zs)
+                            (ws ++ (xs ++ bs ++ ys) ++ zs)
+    go ws xs q ys zs
+     -- (ws ++ ((xs ++ _) ++ ys)) ++ zs
+      = withAssoc @ws @(xs ++ as) @ys
+      $ withAssoc @ws @(xs ++ bs) @ys
+     -- ((ws ++ (xs ++ _)) ++ ys) ++ zs
+      $ withAssoc @ws @xs @as
+      $ withAssoc @ws @xs @bs
+     -- (((ws ++ xs) ++ _) ++ ys) ++ zs
+      $ withAssoc @(ws ++ xs ++ as) @ys @zs
+      $ withAssoc @(ws ++ xs ++ bs) @ys @zs
+     -- ((ws ++ xs) ++ _) ++ (ys ++ zs)
+      $ PremonoidalAtom (appendP ws xs) q (appendP ys zs)
 
 instance Premonoidal (FreePremonoidal q) where
-  first Id _
+  widen _ Id _
     = Id
-  first (q :>>> qs) zs
-        = firstAtom q zs
-     :>>> first qs zs
-  second _ Id
-    = Id
-  second xs (q :>>> qs)
-        = secondAtom xs q
-     :>>> second xs qs
+  widen xs (q :>>> qs) zs
+        = widenAtom xs q zs
+     :>>> widen xs qs zs
 
 
 runPremonoidalAtom
@@ -90,7 +69,7 @@ runPremonoidalAtom
   => (forall xs ys. q xs ys -> r xs ys)
   -> PremonoidalAtom q as bs -> r as bs
 runPremonoidalAtom runQ (PremonoidalAtom xs q ys)
-  = first (second xs (runQ q)) ys
+  = widen xs (runQ q) ys
 
 runFreePremonoidal
   :: Premonoidal r
